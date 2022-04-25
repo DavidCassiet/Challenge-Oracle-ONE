306198818fe574fbae7effcb1e5f932bf53a80c9
David Cassiet <davidcassiet@gmail.com>
2022-04-24 22:24:21 -0300

    Adding validator and animation

--- encryptor.js
+++ encryptor.js
@@ -1,66 +1,102 @@
-let result = document.getElementById("showText");
+let result = document.querySelector("#showText");
+let textDom = document.querySelector("#domText");
+let section = document.querySelector("#hidden");
+let defect = document.querySelector("#defect");
 
 function encrypt() {
-  let text = document.getElementById("domText").value;
+  let text = textDom.value;
   let encryptedText = "";
 
-  for (let i = 0; i < text.length; i++) {
-    let letter = text[i];
+  if (validate(text)) {
+    for (let i = 0; i < text.length; i++) {
+      let letter = text[i];
 
-    if (letter == "a") {
-      letter = "ai";
-    } else if (letter == "e") {
-      letter = "enter";
-    } else if (letter == "i") {
-      letter = "imes";
-    } else if (letter == "o") {
-      letter = "ober";
-    } else if (letter == "u") {
-      letter = "ufat";
+      if (letter == "a") {
+        letter = "ai";
+      } else if (letter == "e") {
+        letter = "enter";
+      } else if (letter == "i") {
+        letter = "imes";
+      } else if (letter == "o") {
+        letter = "ober";
+      } else if (letter == "u") {
+        letter = "ufat";
+      }
+      encryptedText = encryptedText + letter;
     }
-    encryptedText = encryptedText + letter;
+    animation();
+    showText(result, encryptedText);
+  } else {
+    return alert("Por favor ingrese un texto valido");
   }
-
-  showText(result, encryptedText);
 }
 
 function decrypt() {
-  let text = document.getElementById("domText").value;
-  let decryptedText = text;
-
-  let rega = /ai/g;
-  let rege = /enter/g;
-  let regi = /imes/g;
-  let rego = /ober/g;
-  let regu = /ufat/g;
-
-  decryptedText = decryptedText.replace(rega, "a");
-  decryptedText = decryptedText.replace(rege, "e");
-  decryptedText = decryptedText.replace(regi, "i");
-  decryptedText = decryptedText.replace(rego, "o");
-  decryptedText = decryptedText.replace(regu, "u");
-
-  showText(result, decryptedText);
+  if (validate(textDom.value)) {
+    animation();
+    showText(result, decryptText(textDom.value));
+  } else {
+    return alert("Por favor ingrese un texto valido");
+  }
 }
 
-function showText(value, text) {
-  return (value.textContent = text);
+function animation() {
+  defect.classList.add("fadeOut");
+
+  setTimeout(function () {
+    defect.classList.add("invisible");
+    section.classList.add("fadeOut");
+    section.classList.remove("invisible");
+  }, 1000);
+
+  setTimeout(function () {
+    section.classList.remove("fadeOut");
+    section.classList.add("appear");
+  }, 1100);
+}
+
+function decryptText(text) {
+  const dictionary = {
+    a: /ai/g,
+    e: /enter/g,
+    i: /imes/g,
+    o: /ober/g,
+    u: /ufat/g,
+  };
+
+  for (const [key, value] of Object.entries(dictionary)) {
+    text = text.replace(value, key);
+  }
+  return text;
+}
+
+function validate(text) {
+  let isValid = true;
+
+  if (text.length > 0) {
+    for (let i = 0; i < text.length; i++) {
+      let letter = text[i];
+      let newLetter = letter.normalize("NFD").replace(/[\u0300-\u036f]/, "");
+      if (letter != letter.toLowerCase() || letter != newLetter) {
+        isValid = false;
+        break;
+      }
+    }
+  } else {
+    isValid = false;
+  }
+  return isValid;
+}
+
+function showText(element, text) {
+  return (element.textContent = text);
 }
 
 function copy() {
-  let textDommm = document.getElementById("showText");
-  textDommm.select();
+  result.select();
   document.execCommand("copy");
 }
 
 function clearInput() {
-  let textDom = document.getElementById("domText");
   textDom.value = "";
 }
-
-// function copy() {
-//   let textDom = document.getElementById("domText");
-//   let text = document.getElementById("showText").innerHTML;
-
-//   textDom.value = text;
-// }
